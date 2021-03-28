c126631d72745c8ff7639762f20ed0605df8d1bb
Sanyarozetka <81380546+Sanyarozetka@users.noreply.github.com>
2021-03-28 15:26:15 +0300

    First commit

---
+++ Lesson_01/first.py
@@ -0,0 +1,22 @@
+
+
+print("Hello World!")
+
+print(3, 7, 'test', False)
+
+# input()
+
+a = 56
+print(a, type(a))
+
+a = "Hello"
+print(a, type(a))
+
+# a = input('Введите целое число: ')
+# print(a, type(a))
+# b = int(a)
+# print(b, type(b))
+
+a = 546545213545848
+b = 489
+print(a*b)
